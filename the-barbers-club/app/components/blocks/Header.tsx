bc2e47414723f4504a3aeb7e692e5305123cfe30
tonyntje <tony_nieuwenhuijs@live.nl>
2023-12-30 21:53:52 +0100

    New TopBar component for Header and installed Carbon Icons

--- the-barbers-club/app/components/blocks/Header.tsx
+++ the-barbers-club/app/components/blocks/Header.tsx
@@ -1,14 +1,16 @@
 import Image from "next/image";
 import TheBarberClubLogo from '../../../public/TheBarbersClubLogo.svg'
 import {Nav} from "@/app/components/navigation/Nav";
+import {TopBar} from "@/app/components/blocks/TopBar";
 
 export const Header = () => {
-	return (
-		<header className='w-full bg-white border border-b-2 '>
-			<div className='max-w-screen-2xl flex justify-between mx-auto p-6'>
-				<Image width='200' src={TheBarberClubLogo} alt='The Barbers Club Logo'/>
-				<Nav/>
-			</div>
-		</header>
-	)
+  return (
+    <header className='w-full bg-white border border-b-2 '>
+      <TopBar/>
+      <div className='max-w-screen-2xl flex justify-between mx-auto p-6'>
+        <Image width='200' src={TheBarberClubLogo} alt='The Barbers Club Logo'/>
+        <Nav/>
+      </div>
+    </header>
+  )
 }
