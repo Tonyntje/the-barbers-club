172d367e2d4a708e7dc53631174fbb7df13574b0
tonyntje <tony_nieuwenhuijs@live.nl>
2023-12-31 11:20:12 +0100

    Footer new text

--- the-barbers-club/app/components/blocks/Footer.tsx
+++ the-barbers-club/app/components/blocks/Footer.tsx
@@ -1,38 +1,55 @@
 import Image from "next/image";
-import TheBarbersClubFooterLogo from '../../../public/TheBarbersClubFooterLogo.svg'
-import {Column} from "@/app/components/utilities/Column";
+import TheBarbersClubFooterLogo from "../../../public/TheBarbersClubFooterLogo.svg";
+import { Column } from "@/app/components/utilities/Column";
 import Heading from "@/app/components/content/Heading";
-import {Copyright} from "@/app/components/blocks/Copyright";
-import {Button} from "@/app/components/interaction/Button";
+import { Copyright } from "@/app/components/blocks/Copyright";
+import { Button } from "@/app/components/interaction/Button";
 
 const shopTimes = [
-  {time: '10:00 - 16:00', label: 'Maandag'},
-  {time: '09:00 - 17:00', label: 'Dinsdag'},
-  {time: '10:00 - 21:00', label: 'Woensdag'},
-  {time: '09:00 - 21:00', label: 'Donderdag'},
-  {time: '09:00 - 18:00', label: 'Vrijdag'},
-  {time: '08:00 - 18:00', label: 'Zaterdag'},
-  {time: 'Gesloten', label: 'Zondag'},
-]
+  { time: "10:00 - 16:00", label: "Maandag" },
+  { time: "09:00 - 17:00", label: "Dinsdag" },
+  { time: "10:00 - 21:00", label: "Woensdag" },
+  { time: "09:00 - 21:00", label: "Donderdag" },
+  { time: "09:00 - 18:00", label: "Vrijdag" },
+  { time: "08:00 - 18:00", label: "Zaterdag" },
+  { time: "Gesloten", label: "Zondag" },
+];
 
 export const Footer = () => {
-  return <footer className='w-full bg-neutral-800 text-white'>
-    <div className='max-w-screen-2xl flex justify-between mx-auto px-6 py-24'>
-      <Column cols={3}>
-        <div><Image src={TheBarbersClubFooterLogo} alt='The Barbers Club logo in footer'/></div>
-        <div className='col-span-2'>
-          <Heading level={3}>Openingstijden</Heading>
-          <ul className='mb-8 grid grid-cols-2 gap-x-8'>
-            {shopTimes.map(({time, label}) =>
-              <li className='w-full flex justify-between border-b border-neutral-700 py-2' key={label}>
-                <span className='text-accent-200'>{label}</span> {time}
-              </li>
-            )}
-          </ul>
-          <Button type='button' variant='primary' label='Afspraak maken'/>
-        </div>
-      </Column>
-    </div>
-    <Copyright/>
-  </footer>
-}
+  return (
+    <footer className="w-full bg-neutral-800 text-white">
+      <div className="max-w-screen-2xl flex justify-between mx-auto px-6 py-24">
+        <Column cols={3}>
+          <div className="flex flex-col gap-6">
+            <Image
+              src={TheBarbersClubFooterLogo}
+              alt="The Barbers Club logo in footer"
+            />
+            <p>
+              Vind ons op [Adres], bel ons via <a href="tel:+3107012345678"></a>
+              , of stuur een e-mail naar{" "}
+              <a href="mailto:info@thebarbersclub.nl">info@thebarbersclub.nl</a>
+              .
+            </p>
+          </div>
+
+          <div className="col-span-2">
+            <Heading level={3}>Openingstijden</Heading>
+            <ul className="mb-8 grid grid-cols-2 gap-x-8">
+              {shopTimes.map(({ time, label }) => (
+                <li
+                  className="w-full flex justify-between border-b border-neutral-700 py-2"
+                  key={label}
+                >
+                  <span className="text-accent-200">{label}</span> {time}
+                </li>
+              ))}
+            </ul>
+            <Button type="button" variant="primary" label="Afspraak maken" />
+          </div>
+        </Column>
+      </div>
+      <Copyright />
+    </footer>
+  );
+};
