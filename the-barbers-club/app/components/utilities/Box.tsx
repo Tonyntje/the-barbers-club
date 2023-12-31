3ecbda0d98d8864740c74faf374706319dfda4c5
tonyntje <tony_nieuwenhuijs@live.nl>
2023-12-31 11:51:20 +0100

    Edits on Box components

--- the-barbers-club/app/components/utilities/Box.tsx
+++ the-barbers-club/app/components/utilities/Box.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export const Box = ({ children }: BoxType) => {
   return (
-    <div className="p-6 border border-neutral-400 rounded-lg">{children}</div>
+    <div className="p-6 border border-neutral-300 rounded-lg">{children}</div>
   );
 };
 
