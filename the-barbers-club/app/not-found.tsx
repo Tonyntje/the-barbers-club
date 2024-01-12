59da1932abf3ac29917c1c121504f1fc207746f4
tonyntje <tony_nieuwenhuijs@live.nl>
2024-01-12 21:51:09 +0100

    Temporary change for not-found.tsx

--- the-barbers-club/app/not-found.tsx
+++ the-barbers-club/app/not-found.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Button, Heading } from "@/app/components";
+import { useState } from "react";
 
 const ErrorNum = () => {
   return (
@@ -11,7 +12,7 @@ const ErrorNum = () => {
 };
 
 export default function NotFound() {
-  // const pathname = window.location.pathname;
+  const [pathname] = useState(""); //window.location.pathname
 
   return (
     <div className=" gap-4 radial-background w-full h-full sm:min-h-[700px] p-28 flex flex-col items-center justify-center">
@@ -20,7 +21,7 @@ export default function NotFound() {
       <div className="flex items-center gap-2">
         <p>De pagina</p>
         <p className="border py-0.5 px-2 text-neutral-600 border-neutral-300 bg-neutral-100 rounded-lg">
-          {/*{pathname}*/}
+          {pathname}
         </p>
         <p>is niet gevonden</p>
       </div>
