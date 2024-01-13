23827c11a1ddf3a093d906fcfecbb7d1dee2b5da
tonyntje <tony_nieuwenhuijs@live.nl>
2024-01-13 09:43:25 +0100

    Returned folders

--- the-barbers-club/app/components/index.ts
+++ the-barbers-club/app/components/index.ts
@@ -1,5 +1,29 @@
+// Blocks
+export * from "./blocks/TopBar";
+export * from "./blocks/Header";
+export * from "./blocks/HeroBanner";
+export * from "./blocks/Section";
+export * from "./blocks/Footer";
+export * from "./blocks/Copyright";
+
 // Content
 export * from "./content/Heading";
 
 // Interaction
 export * from "./interaction/Button";
+
+// Navigation
+export * from "./navigation/Nav";
+
+// Utilities
+export * from "./utilities/Box";
+export * from "./utilities/Column";
+export * from "./utilities/CenterWrapper";
+export * from "./utilities/Spacer";
+export * from "./utilities/SubBlock";
+
+// Form
+export * from "./form/inputText";
+export * from "./form/inputSelect";
+export * from "./form/inputDate";
+export * from "./booking-form/BookingForm";
