b155d2feee974ce059a9b2fe5b5a040c71e05d9d
tonyntje <tony_nieuwenhuijs@live.nl>
2024-03-24 20:09:05 +0100

    Refactored code and added mobile menu functionality to website

--- the-barbers-club/app/components/blocks/Header.tsx
+++ the-barbers-club/app/components/blocks/Header.tsx
@@ -3,13 +3,26 @@
 import Image from "next/image";
 import TheBarberClubLogo from "../../../public/TheBarbersClubLogo.svg";
 import { Nav } from "@/app/components/navigation/Nav";
-import { TopBar } from "@/app/components/blocks/TopBar";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTrigger,
+  TopBar,
+} from "@/app/components";
+import { useState } from "react";
+import { MenuIcon } from "lucide-react";
 
 export const Header = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const isMobile = windowWidth < 768;
+
+  window.addEventListener("resize", () => setWindowWidth(window.innerWidth));
+
   return (
     <header className="w-full bg-white border border-b-2 ">
       <TopBar />
-      <div className="max-w-screen-2xl flex justify-center items-center sm:justify-between mx-auto p-6">
+      <div className="max-w-screen-2xl flex items-center justify-between mx-auto p-6 w-full">
         <a href="/">
           <Image
             width="200"
@@ -17,9 +30,33 @@ export const Header = () => {
             alt="The Barbers Club Logo"
           />
         </a>
-        <div className="hidden sm:block">
-          <Nav />
-        </div>
+        {isMobile ? (
+          <div className="block sm:hidden">
+            <Sheet key="left">
+              <SheetTrigger>
+                <div className="flex gap-4 font-bold text-xl items-center">
+                  Menu <MenuIcon />
+                </div>
+              </SheetTrigger>
+              <SheetContent className="w-[400px] sm:w-[540px] flex flex-col gap-8">
+                <SheetHeader>
+                  <a className="mx-auto mt-10" href="/">
+                    <Image
+                      width="200"
+                      src={TheBarberClubLogo}
+                      alt="The Barbers Club Logo"
+                    />
+                  </a>
+                </SheetHeader>
+                <Nav />
+              </SheetContent>
+            </Sheet>
+          </div>
+        ) : (
+          <div className="hidden sm:block">
+            <Nav />
+          </div>
+        )}
       </div>
     </header>
   );
