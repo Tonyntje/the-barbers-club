a191f9e065c5eedc1a12be96aeb6265573e1943b
tonyntje <tony_nieuwenhuijs@live.nl>
2024-03-30 15:32:07 +0100

    Replaced cn with classnames

--- the-barbers-club/app/components/form/radio-group.tsx
+++ the-barbers-club/app/components/form/radio-group.tsx
@@ -3,8 +3,7 @@
 import * as React from "react";
 import * as RadioGroupPrimitive from "@radix-ui/react-radio-group";
 import { Circle } from "lucide-react";
-
-import { cn } from "@/lib/utils";
+import classNames from "classnames";
 
 const RadioGroup = React.forwardRef<
   React.ElementRef<typeof RadioGroupPrimitive.Root>,
@@ -12,7 +11,7 @@ const RadioGroup = React.forwardRef<
 >(({ className, ...props }, ref) => {
   return (
     <RadioGroupPrimitive.Root
-      className={cn("grid gap-2", className)}
+      className={classNames("grid gap-2", className)}
       {...props}
       ref={ref}
     />
@@ -27,7 +26,7 @@ const RadioGroupItem = React.forwardRef<
   return (
     <RadioGroupPrimitive.Item
       ref={ref}
-      className={cn(
+      className={classNames(
         "aspect-square h-4 w-4 rounded-full border border-neutral-200 border-neutral-900 text-neutral-900 ring-offset-white focus:outline-none focus-visible:ring-2 focus-visible:ring-neutral-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-neutral-800 dark:border-neutral-50 dark:text-neutral-50 dark:ring-offset-neutral-950 dark:focus-visible:ring-neutral-300",
         className,
       )}
