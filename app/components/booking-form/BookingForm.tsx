e711ec3d05fba00776d6e9852791abc768b150e5
tonyntje <tony_nieuwenhuijs@live.nl>
2024-06-16 16:38:11 +0200

    Changes for working form for whatsapp and phone

--- app/components/booking-form/BookingForm.tsx
+++ app/components/booking-form/BookingForm.tsx
@@ -10,6 +10,8 @@ import { useStore } from "@/app/machine/machine";
 import { DatumTijd } from "@/app/components/booking-form/helpers/steps/DatumTijd";
 import { Informatie } from "@/app/components/booking-form/helpers/steps/Informatie";
 import { Betalen } from "@/app/components/booking-form/helpers/steps/Betalen";
+import phoneIcon from "@/public/phone--incoming.svg";
+import whatsappIcon from "@/public/whatsapp-white.svg";
 
 export const BookingForm = () => {
   const [stepStatus, setStepStatus] = useState(1);
@@ -37,6 +39,8 @@ export const BookingForm = () => {
   }, []);
 
   const isOpen = useStore((state) => state.isOpen);
+  const isDev = useStore((state) => state.isDev);
+
   const setBookingStatus = useStore((state) => state.setBookingStatus);
 
   return (
@@ -50,38 +54,74 @@ export const BookingForm = () => {
             >
               <Subtract size={24} />
             </div>
-            <div className="top-slide-slow">
-              <div className="mx-auto w-fit mb-6">
-                <Image
-                  width="200"
-                  src={TheBarberClubLogo}
-                  alt="The Barbers Club Logo"
-                />
+
+            {isDev ? (
+              <div className="top-slide-slow">
+                <div className="mx-auto w-fit mb-6">
+                  <Image
+                    width="200"
+                    src={TheBarberClubLogo}
+                    alt="The Barbers Club Logo"
+                  />
+                </div>
+                <BookingStatus status={stepStatus} />
+                {stepStatus === 1 && (
+                  <Informatie
+                    control={control}
+                    setStatus={setStepStatus}
+                    name={name}
+                  />
+                )}
+                {stepStatus === 2 && (
+                  <DatumTijd
+                    date={date}
+                    time={time}
+                    setDate={setDate}
+                    setTime={setTime}
+                    setStatus={setStepStatus}
+                  />
+                )}
+                {stepStatus === 3 && (
+                  <Betalen
+                    setStatus={setStepStatus}
+                    orderDetails={orderDetails}
+                  />
+                )}
               </div>
-              <BookingStatus status={stepStatus} />
-              {stepStatus === 1 && (
-                <Informatie
-                  control={control}
-                  setStatus={setStepStatus}
-                  name={name}
-                />
-              )}
-              {stepStatus === 2 && (
-                <DatumTijd
-                  date={date}
-                  time={time}
-                  setDate={setDate}
-                  setTime={setTime}
-                  setStatus={setStepStatus}
-                />
-              )}
-              {stepStatus === 3 && (
-                <Betalen
-                  setStatus={setStepStatus}
-                  orderDetails={orderDetails}
-                />
-              )}
-            </div>
+            ) : (
+              <div className="top-slide-slow text-center">
+                <div className="mx-auto w-fit mb-6">
+                  <Image
+                    width="200"
+                    src={TheBarberClubLogo}
+                    alt="The Barbers Club Logo"
+                  />
+                </div>
+                <b>Wilt u alvast een afspraak maken?</b>
+                <br />
+                <span>Bel of whatsApp naar de Barbers Club.</span>
+                <div className="flex gap-4">
+                  <a
+                    className="flex mt-3 gap-4 text-lg px-6 py-2 text-white items-center justify-center bg-primary-700 hover:bg-primary-600 transition-colors rounded-xl"
+                    href="tel:+31639132769"
+                  >
+                    <div className="bg-primary-800 rounded-full p-2">
+                      <Image width="18" src={phoneIcon} alt="Bel ons" />
+                    </div>
+                    Bellen
+                  </a>
+                  <a
+                    className="flex mt-3 gap-4 text-lg px-6 py-2 text-white items-center justify-center bg-primary-700 hover:bg-primary-600 transition-colors rounded-xl"
+                    href="https://wa.me/31639132769?text=Beste%20Barbersclub%20ik%20zou%20graag%20een%20afspraak%20willen%20maken"
+                  >
+                    <div className="bg-primary-800 rounded-full p-2">
+                      <Image width="18" src={whatsappIcon} alt="WhatsApp" />
+                    </div>
+                    WhatsApp
+                  </a>
+                </div>
+              </div>
+            )}
           </div>
         </form>
       ) : (
