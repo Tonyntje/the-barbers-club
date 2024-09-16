63889348af0bde5daabddde11dd5599c597b25fd
tonyntje <tony_nieuwenhuijs@live.nl>
2024-09-16 18:49:31 +0200

    Removed image from gallery

--- app/the-barbers-club/components/photos.ts
+++ app/the-barbers-club/components/photos.ts
@@ -2,7 +2,6 @@ import image1 from "../../../public/IMG_1572.jpg";
 import image2 from "../../../public/IMG_1638.jpg";
 import image3 from "../../../public/IMG_1639.jpg";
 import image4 from "../../../public/IMG_1641.jpg";
-import image5 from "../../../public/IMG_1129.jpg";
 import image6 from "../../../public/IMG_1642.jpg";
 import image7 from "../../../public/IMG_1753.jpg";
 import image8 from "../../../public/IMG_1755.jpg";
@@ -15,7 +14,6 @@ export const photos = [
   image2,
   image3,
   image4,
-  image5,
   image6,
   image7,
   image8,
