27d6b9ece661bd6ff8853672e7c1d5d0c34afbc3
tony.nieuwenhuijs <tony.nieuwenhuijs@iodigital.com>
2025-03-14 12:05:11 +0100

    New border color

--- app/components/blocks/Header.tsx
+++ app/components/blocks/Header.tsx
@@ -2,63 +2,69 @@
 
 import Image from "next/image";
 import TheBarberClubLogo from "../../../public/TheBarbersClubLogo.svg";
-import {Nav} from "@/app/components/navigation/Nav";
-import {Sheet, SheetContent, SheetHeader, SheetTrigger, TopBar,} from "@/app/components";
-import {useEffect, useState} from "react";
-import {MenuIcon} from "lucide-react";
+import { Nav } from "@/app/components/navigation/Nav";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTrigger,
+  TopBar,
+} from "@/app/components";
+import { useEffect, useState } from "react";
+import { MenuIcon } from "lucide-react";
 import Link from "next/link";
 
 export const Header = () => {
-	const [windowWidth, setWindowWidth] = useState(1000);
-	const isMobile = windowWidth < 768;
+  const [windowWidth, setWindowWidth] = useState(1000);
+  const isMobile = windowWidth < 768;
 
-	useEffect(() => {
-		getWindowWidth();
+  useEffect(() => {
+    getWindowWidth();
 
-		if (typeof window !== "undefined")
-			window.addEventListener("resize", () => getWindowWidth());
-	}, []);
+    if (typeof window !== "undefined")
+      window.addEventListener("resize", () => getWindowWidth());
+  }, []);
 
-	const getWindowWidth = () => {
-		setWindowWidth(window.innerWidth);
-	};
+  const getWindowWidth = () => {
+    setWindowWidth(window.innerWidth);
+  };
 
-	return (
-		<header className="w-full bg-white border border-b-2 ">
-			<TopBar />
-			<div className="max-w-screen-2xl flex items-center justify-between mx-auto p-6 w-full">
-				<Link href="/">
-					<Image
-						width="200"
-						src={TheBarberClubLogo}
-						alt="The Barbers Club Logo"
-						priority
-					/>
-				</Link>
-				{isMobile ? (
-					<Sheet key="left">
-						<SheetTrigger>
-							<div className="flex gap-4 font-bold text-xl items-center">
-								Menu <MenuIcon />
-							</div>
-						</SheetTrigger>
-						<SheetContent className="w-[400px] sm:w-[540px] flex flex-col gap-8">
-							<SheetHeader>
-								<Link className="mx-auto mt-10" href="/">
-									<Image
-										width="200"
-										src={TheBarberClubLogo}
-										alt="The Barbers Club Logo"
-									/>
-								</Link>
-							</SheetHeader>
-							<Nav />
-						</SheetContent>
-					</Sheet>
-				) : (
-					<Nav />
-				)}
-			</div>
-		</header>
-	);
+  return (
+    <header className="w-full border-b-2 border-gray-300 bg-white">
+      <TopBar />
+      <div className="mx-auto flex w-full max-w-screen-2xl items-center justify-between p-6">
+        <Link href="/">
+          <Image
+            width="200"
+            src={TheBarberClubLogo}
+            alt="The Barbers Club Logo"
+            priority
+          />
+        </Link>
+        {isMobile ? (
+          <Sheet key="left">
+            <SheetTrigger>
+              <div className="flex items-center gap-4 text-xl font-bold">
+                Menu <MenuIcon />
+              </div>
+            </SheetTrigger>
+            <SheetContent className="flex w-[400px] flex-col gap-8 sm:w-[540px]">
+              <SheetHeader>
+                <Link className="mx-auto mt-10" href="/">
+                  <Image
+                    width="200"
+                    src={TheBarberClubLogo}
+                    alt="The Barbers Club Logo"
+                  />
+                </Link>
+              </SheetHeader>
+              <Nav />
+            </SheetContent>
+          </Sheet>
+        ) : (
+          <Nav />
+        )}
+      </div>
+    </header>
+  );
 };
